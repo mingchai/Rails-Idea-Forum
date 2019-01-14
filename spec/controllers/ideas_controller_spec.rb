c662f7545feeb1e1d6afceb25a399d8d31399b16
Ming Chai <ming.chai4@gmail.com>
2019-01-14 10:40:13 -0800

    complete tests for new and create actions. 6 total

--- spec/controllers/ideas_controller_spec.rb
+++ spec/controllers/ideas_controller_spec.rb
@@ -13,5 +13,30 @@ RSpec.describe IdeasController, type: :controller do
     end
 
     describe "#create" do
+        context 'valid input' do
+            it 'sets an instance variable to a new instance of an Idea' do
+                post(:create, params: {idea: FactoryBot.attributes_for(:idea)})
+
+                expect(assigns(:idea)).to be_a(Idea)
+            end
+            it 'adds an entry to the db' do
+                count_before = Idea.count
+                post(:create, params: {idea: FactoryBot.attributes_for(:idea)})
+                count_after = Idea.count
+
+                expect(count_after).to eq(count_before + 1)
+            end
+            it 'redirects the user to the index, home, or show page' do
+                post(:create, params: {idea: FactoryBot.attributes_for(:idea)})
+
+                expect(response).to redirect_to(ideas_url)
+            end
+        end
+        context 'invalid inputs' do
+            it 'renders the new template' do
+                post(:create, params: {idea: FactoryBot.attributes_for(:idea, title: nil)})
+                expect(response).to render_template(:new)
+            end
+        end
     end
 end
